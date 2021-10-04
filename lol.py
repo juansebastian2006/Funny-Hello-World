2fb5ceef303b26a034fd0820d718a50452445403
Juan Sebastián <89955811+juansebastian2006@users.noreply.github.com>
2021-10-04 00:17:38 -0300

    Create lol.py

---
+++ lol.py
@@ -0,0 +1,50 @@
+for a in ''.join(
+    b for b in ''.join(
+        c for c in ''.join(
+            d for d in ''.join(
+                e for e in ''.join(
+                    f for f in ''.join(
+                        g for g in ''.join(
+                            h for h in ''.join(
+                                i for i in ''.join(
+                                    j for j in ''.join(
+                                        k for k in ''.join(
+                                            l for l in ''.join(
+                                                m for m in ''.join(
+                                                    n for n in ''.join(
+                                                        o for o in ''.join(
+                                                            p for p in ''.join(
+                                                                q for q in ''.join(
+                                                                    r for r in ''.join(
+                                                                        s for s in ''.join(
+                                                                            t for t in ''.join(
+                                                                                u for u in ''.join(
+                                                                                    v for v in ''.join(
+                                                                                        w for w in ''.join(
+                                                                                            x for x in ''.join(
+                                                                                                y for y in ''.join(z for z in ''.join(chr(int(i)) for i in "72 101 108 108 111 32 87 111 114 108 100".split()
+                                                                                                )
+                                                                                            )
+                                                                                        )
+                                                                                    )
+                                                                                )
+                                                                            )
+                                                                        )
+                                                                    )
+                                                                )
+                                                            )
+                                                        )
+                                                    )
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    ))):                                                                                                                                                                                                                                                                                                                                                                                              
+    print(a, end="")
